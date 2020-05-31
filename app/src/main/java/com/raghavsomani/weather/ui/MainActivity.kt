10e6e7b8c226ce5c6fb5ac23f0cf9f0d126b37f5
raghavsomani21 <raghav2102somani@gmail.com>
2020-05-31 18:09:53 +0530

    Video1 Location dependency problem removed it

---
+++ app/src/main/java/com/raghavsomani/weather/ui/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.raghavsomani.weather.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.raghavsomani.weather.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+        bottom_nav.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+
+    }
+
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp()
+    }
+
+}
---
+++ app/src/main/java/com/raghavsomani/weather/ui/weather/future/details/FutureDetailsWeatherFragment.kt
@@ -0,0 +1,34 @@
+package com.raghavsomani.weather.ui.weather.future.details
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.raghavsomani.weather.R
+
+class FutureDetailsWeatherFragment : Fragment() {
+
+    companion object {
+        fun newInstance() =
+            FutureDetailsWeatherFragment()
+    }
+
+    private lateinit var viewModel: FutureDetailsWeatherViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.future_details_weather_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(FutureDetailsWeatherViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
